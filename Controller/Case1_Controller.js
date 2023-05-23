7df120d3aa2e8e91608e0a149d68f6668798c8ea
FILIPRAIDER <filipraider123@gmail.com>
2023-05-22 21:14:08 -0500

    ejemplo del caso 1

--- Controller/Case1_Controller.js
+++ Controller/Case1_Controller.js
@@ -6,39 +6,55 @@ const PResultado = document.getElementById("resultado")
 
 btnCalcular.addEventListener('click', calcular)
 
-function calcular(){
+function calcular() {
     const operacion = txtOperacion.value
     const op1 = parseFloat(txtOp1.value)
     const op2 = parseFloat(txtOp2.value)
- 
+
     //Al Frente
     let V = 340
-    
-    
 
-   
-    
 
-    if((operacion == "Doppler 1 Frente" || "Doppler 1 Atras" ) && !isNaN(op1) && !isNaN(op2)){
+
+
+
+
+    if ((operacion == "Doppler 1 Frente" || "Doppler 1 Atras") && !isNaN(op1) && !isNaN(op2)) {
         let ResultadoFrente;
         let ResultadoAtras;
-        let Result 
-        
-        switch (operacion){
+        let Result
+
+        switch (operacion) {
             case "Doppler 1 Frente":
-               ResultadoFrente = V - op1
-               Result = ResultadoFrente / op2
- 
-               break;
+                ResultadoFrente = V - op1
+                Result = ResultadoFrente / op2
+
+                break;
             case "Doppler 1 Atras":
-               ResultadoAtras = V + op1
-               Result = ResultadoAtras / op2
+                ResultadoAtras = V + op1
+                Result = ResultadoAtras / op2
                 break;
         }
         PResultado.style = "color:black"
         PResultado.innerText = "λ= " + Result.toFixed(2) + " m "
-    }else{
-       PResultado.style = "color:red"
+    } else {
+        PResultado.style = "color:red"
         PResultado.innerText = "Calculo imposible"
-   }
-  }
+    }
+}
+
+function Persona() {
+    const operacion = txtOperacion.value;
+    if (operacion == "Doppler 1 Atras") {
+        document.getElementById("persona1").classList.add("persona1-show");
+        document.getElementById("persona2").classList.remove("persona2-show");
+
+    } else if (operacion == "Doppler 1 Frente") {
+        document.getElementById("persona1").classList.remove("persona1-show");
+        document.getElementById("persona2").classList.add("persona2-show");
+
+    } else {
+        document.getElementById("persona1").classList.remove("persona1-show");
+        document.getElementById("persona2").classList.remove("persona2-show");
+    }
+}
