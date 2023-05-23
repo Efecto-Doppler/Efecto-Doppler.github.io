1854ec2cec391d87f4e5a1c4c53c8bad24ff9bd1
Heiner09 <arias.heiner09@gmail.com>
2023-05-22 22:02:33 -0500

    Actualizacion caso 3

--- Controller/Case3_Controller.js
+++ Controller/Case3_Controller.js
@@ -1,17 +1,18 @@
-const VELOCIDAD_SONIDO = 343;
-// Función para calcular la frecuencia observada
-function frecuenciaReceptor(frecuenciaEmisor, velocidadReceptor) {
-    return frecuenciaObservada;
-  }
+const buttonCalcular = document.getElementById('calcular-btn');
 
-const buttonCalcular = document.getElementById('calcular-btn')
-
-buttonCalcular.onclick = (() => {
+buttonCalcular.onclick = () => {
+  const velocidadSonido = 343;// Velocidad del sonido en m/s
   const velocidadReceptor = document.getElementById('velocidad-receptor').value
   const frecuenciaEmisor = document.getElementById('frecuencia-emisor').value
   const tipoSonido = document.getElementById('tipo-sonido').value
+  let frecuenciaReceptor;
+  if(tipoSonido == 'a'){
+    frecuenciaReceptor = frecuenciaEmisor * (velocidadSonido + (velocidadReceptor))/velocidadSonido;
+  }else if(tipoSonido == 'b'){
+    frecuenciaReceptor = frecuenciaEmisor * (velocidadSonido + (velocidadReceptor * -1))/velocidadSonido;
+  }else{
+    frecuenciaReceptor = 'Tipo de sonido invalido';
+  }
+  console.log("La frecuencia del receptor es: ", frecuenciaReceptor," Hz");
 
-  console.log(velocidadReceptor)
-  console.log(frecuenciaEmisor)
-  console.log(tipoSonido)
-})
+}
